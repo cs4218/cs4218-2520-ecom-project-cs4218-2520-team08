bcf428709efa59b83d14ffcbed5aee7b8c5421a9
Keagan <keaganpzh@gmail.com>
2026-02-05 13:40:13 +0800

    fix: Temporary fix for jest config

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -6,11 +6,11 @@ module.exports = {
   testEnvironment: "node",
 
   // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+  testMatch: ["<rootDir>/controllers/*.test.js", "<rootDir>/models/*.test.js", "<rootDir>/config/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["controllers/**"],
+  collectCoverageFrom: ["controllers/**", "models/**", "config/**"],
   coverageThreshold: {
     global: {
       lines: 100,
